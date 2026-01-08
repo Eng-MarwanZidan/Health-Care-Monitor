4b35d654a80782663e07561948bf230d4731e435
marwan01110 <marwanmohamed@feng.bu.edu.eg>
2026-01-08 14:49:42 +0200

    docker files + runtime

--- Backend/backend/settings/base.py
+++ Backend/backend/settings/base.py
@@ -133,12 +133,7 @@ SIMPLE_JWT = {
 }
 
 # CORS settings: https://pypi.org/project/django-cors-headers/
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173',
-]
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
